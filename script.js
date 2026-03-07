072f966ac940220b96b581a6cce1676d181648d3
Hamed Abd <109720218+HamedMohammedAbd@users.noreply.github.com>
2026-03-07 20:06:42 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,3 @@
+document.getElementById("btn").addEventListener("click", () => {
+    document.getElementById("msg").innerHTML = `Hello I am <b>Hamed Abdelrahman</b> a student in Cloud Computing course.`;
+});
